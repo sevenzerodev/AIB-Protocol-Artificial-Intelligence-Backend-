4d59aae76067c0b2feebf2d4130acbc3cfea6576
SevenZero <sevenzerodev@gmail.com>
2026-05-05 16:36:18 +0200

    Quick commit: Update AIB/src/Loader.php

--- AIB/src/Loader.php
+++ AIB/src/Loader.php
@@ -1,1 +1,9 @@
-
+<?php
+class Loader extends PluginBase
+public function onEnable(){
+  self::$instance = $this;
+  $this->saveDefaultConfig();
+  $this->config = $this->getConfig();
+  $this->registeeBuiltinModels();
+  $this->getServer()->getCommandMap()->register("aib", new aib\command\AIBCommand($this));
+}
