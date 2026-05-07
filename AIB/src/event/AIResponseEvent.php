4aacdaef3c008616a933731ab8badcf1cde4f9b0
SevenZero <sevenzerodev@gmail.com>
2026-05-07 19:35:30 +0200

    Quick commit: Update AIB/src/event/AIResponseEvent.php

---
+++ AIB/src/event/AIResponseEvent.php
@@ -0,0 +1,28 @@
+<?php
+namespace event;
+
+class AIResponseEvent extends pocketmine\event\plugin\PluginEvent{
+
+    public static $handlerList;
+    private $prompt;
+    private $response;
+    private $error;
+    public function __construct(\Loader $plugin, $prompt, $response, $error){
+        parent::__construct($plugin);
+        $this->prompt = $prompt;
+        $this->response = $response;
+        $this->error = $error;
+    }
+    public function getPrompt(){
+        return $this->prompt;
+    }
+    public function getResponse(){
+        return $this->response;
+    }
+    public function getError(){
+        return $this->error;
+    }
+    public function hasError(){
+        return $this->error !== null;
+    }
+}
