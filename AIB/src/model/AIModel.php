6bc0f3c858b3ea0654468d3b12cfdc1d6d653e7f
SevenZero <sevenzerodev@gmail.com>
2026-05-10 04:58:27 +0200

    update AIModel.php

--- AIB/src/model/AIModel.php
+++ AIB/src/model/AIModel.php
@@ -1,11 +1,12 @@
 <?php
-
 namespace model;
-interface AIModel{
 
+interface AIModel {
     public function getModelId();
     public function getProviderName();
+    public function getCodeName();
     public function query($prompt, $systemPrompt = null);
+    public function queryWithHistory($prompt, $systemPrompt, $history);
     public function getApiKey();
     public function isAvailable();
 }
